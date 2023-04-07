71f5d927f1cf7ec5d168475b5e8c523e123ee18a
TL_SW_001 <TL_SW_001@TL_SW_CMA>
2023-04-07 14:44:57 +0630

    Add project files.

---
+++ TestApi/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestApi.Models;
+
+namespace TestApi.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.ToList();
+            }
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.Userid == id);
+            }
+            //return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
