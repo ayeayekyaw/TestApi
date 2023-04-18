218dec469734abd8f2913cc9e1dd2838d089975c
Aye Aye Kyaw <ayeayekyaw.cu3@gmail.com>
2023-04-18 10:50:11 +0630

    CRUD for Employee

--- TestApi/Controllers/EmployeeController.cs
+++ TestApi/Controllers/EmployeeController.cs
@@ -11,12 +11,13 @@ namespace TestApi.Controllers
 {
     public class EmployeeController : ApiController
     {
+        EmployeeDBContext dbContext = new EmployeeDBContext();
         // GET api/<controller>
         public IEnumerable<Employee> Get()
         {
-            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            using (EmployeeDBContext db = new EmployeeDBContext())
             {
-                return dbContext.Employees.ToList();
+                return db.Employees.ToList();
             }
             //return new string[] { "value1", "value2" };
         }
@@ -24,27 +25,66 @@ namespace TestApi.Controllers
         // GET api/<controller>/5
         public Employee Get(int id)
         {
-            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            using (EmployeeDBContext db = new EmployeeDBContext())
             {
-                return dbContext.Employees.FirstOrDefault(e => e.Userid == id);
+                return db.Employees.FirstOrDefault(e => e.Userid == id);
             }
-            //return "value";
         }
 
         // POST api/<controller>
-        public void Post([FromBody] string value)
+        public HttpResponseMessage Post([FromBody] Employee emp)
         {
-
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    dbContext.Employees.Add(emp);
+                    dbContext.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Invalid Model");
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
         }
-
+        [HttpPut]
         // PUT api/<controller>/5
-        public void Put(int id, [FromBody] string value)
-        {
+        public Employee Put(int Id,[FromBody] Employee emp)
+        {          
+                var obj = dbContext.Employees.Where(n => n.Userid == Id).SingleOrDefault();
+                if (obj != null)
+                {
+                obj.Username = emp.Username;
+                obj.Usermail = emp.Usermail;
+                obj.Password = emp.Password;
+                obj.MartialStatus = emp.MartialStatus;
+                obj.Gender = emp.Gender;
+                dbContext.SaveChanges();
+                    
+                }
+            return obj;
         }
 
         // DELETE api/<controller>/5
-        public void Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            try
+            {
+                var obj = dbContext.Employees.Find(id);
+                dbContext.Employees.Remove(obj);
+                dbContext.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+
         }
     }
 }
