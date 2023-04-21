419c2aae3d927cf976a7843806b151c11815a303
Aye Aye Kyaw <ayeayekyaw.cu3@gmail.com>
2023-04-21 13:48:19 +0630

    AddModel

---
+++ TestApi/Controllers/PassportController.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web.Http;
+using TestApi.Models;
+
+namespace TestApi.Controllers
+{
+    public class PassportController : ApiController
+    {
+        MySqlConnection conn = new MySqlConnection("server=192.168.11.75;port=3306;username=root;password=r00tpass;");
+
+        //[HttpGet]
+        //api/<controller>
+        //public string FindById(string id)
+        //{
+        //    conn.Open();
+        //    MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM tbl_citizen LEFT JOIN tbl_appliedpp ON tbl_citizen.Citizen_No=tbl_appliedpp.Citizen_No;", conn);
+
+        //    da.SelectCommand.CommandType = CommandType.Text;
+
+        //    DataTable dt = new DataTable();
+
+        //    da.Fill(dt);
+
+        //    if (dt.Rows.Count > 0)
+        //    {
+        //        return JsonConvert.SerializeObject(dt);
+        //    }
+        //    else
+        //    {
+        //        return "No data found";
+        //    }
+        //}
+        [HttpGet]
+        //api/<controller>
+        public DataTable FindById(string id)
+        {
+            DataTable dt;
+            conn.Open();
+            using (MySqlCommand cmd = new MySqlCommand("SELECT Name,Name_In_Myanmar,Date_Of_Birth,NrcNo,tbl_appliedpp.DEO_No,Passport_No,Date_Of_Issue,Date_Of_Expiry,application_id,IssuingOffice FROM passbaseprint.tbl_citizen LEFT JOIN passbaseprint.tbl_appliedpp ON tbl_citizen.Citizen_No=tbl_appliedpp.Citizen_No LEFT JOIN passbase.tblduration ON tbl_appliedpp.DEO_No=tblduration.DEO_No WHERE tbl_appliedpp.Passport_No= '" + id + "'", conn))
+            {
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                conn.Close();
+                return dt;
+            }
+        }
+    }
+}
---
+++ TestApi/Models/PassportModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TestApi.Models
+{
+    [Table("tbl_appliedpp")]
+    public class PassportModel
+    {
+        [Key]
+        public int AppliedPP_ID { get; set; }
+        public string Citizen_No { get; set; }
+        public string DEO_No { get; set; }
+        public string Occupation { get; set; }
+        public string Residence { get; set; }
+        public string Education { get; set; }
+        public string Purpose_Of_Visit { get; set; }
+        public string Apply_Country { get; set; }
+        public string Voucher_No { get; set; }
+        public string Photo { get; set; }
+        public string Photo_Sunglass { get; set; }
+        public string Signature { get; set; }
+        public string Invoice_No { get; set; }
+        public DateTime Invoice_Date { get; set; }
+        public string Invoice_Amt { get; set; }
+        public string Passport_No { get; set; }
+        public string Old_Passport_No { get; set; }
+        public string PP_Type { get; set; }
+        public DateTime Date_Of_Issue { get; set; }
+        public DateTime Date_Of_Expiry { get; set; }
+        public string Returned_Place { get; set; }
+        public DateTime Returned_Date { get; set; }
+        public int Is_Printed { get; set; }
+        public DateTime Printed_Date { get; set; }
+        public int Is_Approved { get; set; }
+        public DateTime Approved_Date { get; set; }
+        public int Is_Issued { get; set; }
+        public DateTime Issued_Date { get; set; }
+        public DateTime OldPP_Issued_Date { get; set; }
+        public string Issued_By { get; set; }
+        public int Is_Stopped { get; set; }
+        public DateTime Stopped_Date { get; set; }
+        public string Stopped_Reason { get; set; }
+        public int IsDeleted { get; set; }
+        public int IsActived { get; set; }
+        public int IsLocked { get; set; }
+        public DateTime Created_Date { get; set; }
+        public DateTime Modified_Date { get; set; }
+        public int User_ID { get; set; }
+        public int Parking_ID { get; set; }
+        public string Park_No { get; set; }
+        public int Is_Exported { get; set; }
+        public int Is_Mobile { get; set; }
+        public DateTime Carrying_Date { get; set; }
+        public string IssuingOffice { get; set; }
+        public string division { get; set; }
+        public int IsExportedMT { get; set; }
+        public int IsAllowToPrint { get; set; }
+    }
+}
