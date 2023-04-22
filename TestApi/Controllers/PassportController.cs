d81a3f6c41f62ee33cfd99152b2ebec6e214fbb5
Aye Aye Kyaw <ayeayekyaw.cu3@gmail.com>
2023-04-22 13:28:18 +0630

    DBChanges

--- TestApi/Controllers/PassportController.cs
+++ TestApi/Controllers/PassportController.cs
@@ -1,5 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
+//using MySqlConnector;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Web.Http;
@@ -9,7 +11,7 @@ namespace TestApi.Controllers
 {
     public class PassportController : ApiController
     {
-        MySqlConnection conn = new MySqlConnection("server=192.168.11.75;port=3306;username=root;password=r00tpass;");
+        //MySqlConnection conn = new MySqlConnection("server=192.168.11.75;port=3306;username=root;password=r00tpass;");
 
         //[HttpGet]
         //api/<controller>
@@ -38,6 +40,9 @@ namespace TestApi.Controllers
         public DataTable FindById(string id)
         {
             DataTable dt;
+       
+            string con = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            MySqlConnection conn = new MySqlConnection(con);
             conn.Open();
             using (MySqlCommand cmd = new MySqlCommand("SELECT Name,Name_In_Myanmar,Date_Of_Birth,NrcNo,tbl_appliedpp.DEO_No,Passport_No,Date_Of_Issue,Date_Of_Expiry,application_id,IssuingOffice FROM passbaseprint.tbl_citizen LEFT JOIN passbaseprint.tbl_appliedpp ON tbl_citizen.Citizen_No=tbl_appliedpp.Citizen_No LEFT JOIN passbase.tblduration ON tbl_appliedpp.DEO_No=tblduration.DEO_No WHERE tbl_appliedpp.Passport_No= '" + id + "'", conn))
             {
